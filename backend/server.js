fa94114240593c6c36166deb090e1ed866790686
kenshien <kenshienworkacc@gmail.com>
2026-03-14 09:09:44 +0800

    initial:

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,6 @@ app.use("/api/client", require("./routes/client.routes"))
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
+app.listen(PORT, "[IP_ADDRESS]", () => {
     console.log(`Server is running on port ${PORT}`);
 });
