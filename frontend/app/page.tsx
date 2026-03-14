e8aeee030f3b71eb39c346b5ef6c9500fbe55e5a
kenshien <kenshienworkacc@gmail.com>
2026-03-14 18:29:40 +0800

    chore: just change the url

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 export default function Start() {
   const router = useRouter()
   useEffect(() => {
-    router.replace("/page/dashboard")
+    router.replace("/auth/login")
   }, [router])
   return null;
 
