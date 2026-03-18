065c6d80fad6db66c4c24ecf492a9af3d4c33de3
kenshien <kenshienworkacc@gmail.com>
2026-03-18 17:54:27 +0800

    try

--- frontend/src/app/product/page.tsx
+++ frontend/src/app/product/page.tsx
@@ -1,8 +1,5 @@
 import { Suspense } from "react";
 import ProductPage from "./ProductPage";
-
-export const dynamic = "force-dynamic";
-
 export default function Page() {
   return (
     <Suspense>
